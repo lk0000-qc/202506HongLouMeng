be0473e116abbd13c84dd151847bfff4245843d3
kunkun <3362493873@qq.com>
2026-07-17 17:29:50 +0800

    前四回已经写完了

---
+++ MyGit_Demo/MyGit_Demo/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyGit_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // 第一回：甄士隐找了个算卦的 贾雨村跑的真快
+
+            // 第二回：贾夫人仙逝扬州城 冷子兴演说荣国府
+
+            // 第三回：贾雨村夤缘复旧职 林黛玉抛父进京都
+
+            // 第四回：薄命女偏逢薄命郎 葫芦僧乱判葫芦案
+
+        }
+    }
+}
